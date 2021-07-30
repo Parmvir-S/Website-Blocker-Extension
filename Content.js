c4998baed72a7020a908d1700994a1a72bfdc42a
Parmvir-S <parmvir.shergill@gmail.com>
2021-07-30 15:36:34 -0700

    Craigslist Blocking Extension

---
+++ Content.js
@@ -0,0 +1,3 @@
+let img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbNxUSr82AdpiRwrMUT6bM5K2SVb8zzhpaxQizA-egrkb5Kik2yp1WchHXPUJAe2UenrU&usqp=CAU"
+document.body.style.cssText = `text-align: center; margin-top: 15%; font-size: 4.5em;`
+document.body.innerHTML = `<h1> ਕੁਲਦੀਪ ਮੋਟਾ ਮੁੰਡਾ </h1> <br> <img src="${img}">`
